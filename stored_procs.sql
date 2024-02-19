74a9034738a0339c7a3966bb95e7155eb01b4142
omofolawa Alimi-Adeniran <122090537+Omofolawa@users.noreply.github.com>
2024-02-19 22:45:25 +0000

    stored procedure syntaxes

---
+++ stored_procs.sql
@@ -0,0 +1,45 @@
+-- STORED PROCEDURES --
+
+-- TASK1 --
+DELIMITER //
+
+CREATE PROCEDURE GetMaxQuantity()
+BEGIN
+    SELECT MAX(quantity) AS Max_Ordered_Quantity
+    FROM Orders;
+END //
+
+DELIMITER ;
+
+CALL GetMaxOrderedQuantity();
+
+ 
+-- TASK2 --
+
+DELIMITER //
+
+CREATE PROCEDURE GetOrderDetail(IN CustomerID INT)
+BEGIN
+    SET @CustomerID = CustomerID;
+    SET @sql = CONCAT('SELECT OrderID, Quantity, Totalcost FROM Orders WHERE CustomerID = ?');
+    PREPARE stmt FROM @sql;
+    EXECUTE stmt USING @CustomerID;
+    DEALLOCATE PREPARE stmt;
+END //
+
+DELIMITER ;
+ 
+CALL GetOrderDetail(1);
+ 
+-- TASK3 --
+
+DELIMITER //
+
+CREATE PROCEDURE CancelOrder (IN OrderID INT)
+BEGIN
+    DELETE FROM Orders WHERE OrderID = OrderID;
+END //
+
+DELIMITER ;
+ 
+CALL CancelOrder(1);
